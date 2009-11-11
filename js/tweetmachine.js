c14360d281ed03973e0780ea7bfa469b7f7360c3
Matt Wood <doowttam@gmail.com>
2009-11-10 21:05:51 -0600

    Added jquery timers plugin and set up appropariate timers for getting tweets.

--- js/tweetmachine.js
+++ js/tweetmachine.js
@@ -1,15 +1,39 @@
 // Global variables
 var tweetQueue = new Array();
+var lastTweetId = 1;
 
 // Initialization
 $.ajaxSetup({
     username: '',
     password: '',
-    type: 'json'
 });
 
-$.getJSON('http://twitter.com/statuses/friends_timeline.json', function(tweets) {
-    $.each( tweets, function(i, tweet) {
-        $('#tweets').append('<li>' + tweet.text + '</li>');
-    } );
-});
+function getNewTweets() {
+    air.trace('getting new tweets');
+    $.getJSON('http://twitter.com/statuses/friends_timeline.json?since_id=' + lastTweetId, function(tweets) {
+        tweets.reverse();
+        tweetQueue = tweetQueue.concat(tweets);
+        lastTweetId = tweets[tweets.length - 1].id;
+    });
+}
+
+function centerNewTweet() {
+    air.trace('centering tweet');
+    var newTweet = tweetQueue.shift();
+    if ( typeof newTweet == 'undefined' ) {
+        return;
+    }
+    $('#tweets').prepend('<li>' + newTweet.user.screen_name + ' - ' + newTweet.text + '</li>');
+    updateQueueLength();
+}
+
+function updateQueueLength() {
+    air.trace('updateQueueLength:' + tweetQueue.length);
+    $('#queueLength').text(tweetQueue.length);
+}
+
+getNewTweets();
+
+$(document).everyTime(30000, function() { centerNewTweet(); }, 0); 
+
+$(document).everyTime(300000, function() { getNewTweets(); }, 0);
