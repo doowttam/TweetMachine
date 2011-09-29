e65c37c2d29ac4b117c751e7c70a4b9425239fdf
Matt Wood <doowttam@gmail.com>
2011-09-28 21:05:44 -0500

    Adding twitter search methods

--- js/tweetmachine.js
+++ js/tweetmachine.js
@@ -1,6 +1,11 @@
+// Settings
+var searchQuery = '#como';
+var searchMode  = false;
+
 // Global variables
-var tweetQueue = new Array();
-var lastTweetId = 1;
+var tweetQueue   = [];
+var lastTweetId  = 1;
+var tweetBuilder = null;
 
 var oauth_options = {
     consumerKey: '',
@@ -11,7 +16,7 @@ var oauth_options = {
 
 var oauth = OAuth(oauth_options);
 
-function buildTweetHTML(tweet) {
+function buildTimelineTweetHTML(tweet) {
     return "<div class='tweet'>"
         + "<div class='avatar'><img src='" + tweet.user.profile_image_url + "' /></div>"
         + "<div class='tweetBody'><span class='username'>"
@@ -19,22 +24,55 @@ function buildTweetHTML(tweet) {
         + tweet.text + "</span></div></div>";
 }
 
+function buildSearchTweetHTML(tweet) {
+    return "<div class='tweet'>"
+        + "<div class='avatar'><img src='" + tweet.profile_image_url + "' /></div>"
+        + "<div class='tweetBody'><span class='username'>"
+        + tweet.from_user + "</span><span class='tweetText'>"
+        + tweet.text + "</span></div></div>";
+}
+
+function getTimeline(populate) {
+    air.trace('Getting new timeline tweets');
+    oauth.get('http://api.twitter.com/1/statuses/home_timeline.json?since_id=' + lastTweetId, function(data) {
+        var tweets = $.parseJSON(data.text);
+        queueTweets(tweets, populate);
+    });
+}
+
+function getSearch(populate) {
+    air.trace('Getting new search tweets');
+    var url = 'http://search.twitter.com/search.json?q=' + encodeURIComponent(searchQuery) + '&rpp=30&lang=en&since_id=' + lastTweetId;
+    $.getJSON(url, function(data) {
+        var tweets = data.results;
+        queueTweets(tweets, populate);
+    });
+}
+
+function queueTweets(tweets, populate) {
+    if (tweets.length > 1) {
+        tweets.reverse();
+        tweetQueue = tweetQueue.concat(tweets);
+        lastTweetId = tweets[tweets.length - 1].id;
+    } else if ( tweets.length > 0 ) {
+        lastTweetId = tweets[0].id;
+    }
+    if (populate == 1) {
+        initialPopulate();
+    }
+}
+
 function getNewTweets(populate) {
     air.trace('Getting new tweets');
-    oauth.get('http://api.twitter.com/1/statuses/home_timeline.json?since_id=' + lastTweetId, function(data) {
-        air.Introspector.Console.log(data);
-        var tweets = $.parseJSON(data.text);
-        if (tweets.length > 1) {
-            tweets.reverse();
-            tweetQueue = tweetQueue.concat(tweets);
-            lastTweetId = tweets[tweets.length - 1].id;
-        } else if ( tweets.length > 0 ) {
-            lastTweetId = tweets[0].id;
-        }
-        if (populate == 1) {
-            initialPopulate();
-        }
-    });
+
+    if ( searchMode ) {
+        tweetBuilder = buildSearchTweetHTML;
+        getSearch(populate);
+    }
+    else {
+        tweetBuilder = buildTimelineTweetHTML;
+        getTimeline(populate);
+    }
 }
 
 function initialPopulate() {
@@ -44,13 +82,13 @@ function initialPopulate() {
 
 
     $.each( bottomTweets, function(index, tweet) {
-        $('.timelineBOTTOM').prepend(buildTweetHTML(tweet));
+        $('.timelineBOTTOM').prepend(tweetBuilder(tweet));
     });
 
-    $('.centerTweet').prepend(buildTweetHTML(centerTweet));
+    $('.centerTweet').prepend(tweetBuilder(centerTweet));
 
     $.each( topTweets, function(index, tweet) {
-        $('.timelineTOP').prepend(buildTweetHTML(tweet));
+        $('.timelineTOP').prepend(tweetBuilder(tweet));
     });
 
     $('.tweet').show();
@@ -64,7 +102,7 @@ function rotateTweets() {
         return;
     }
 
-    $('.timelineTOP').prepend(buildTweetHTML(newTweet));
+    $('.timelineTOP').prepend(tweetBuilder(newTweet));
     $('.timelineTOP > .tweet:last-child').clone().css('display', 'none').prependTo('.centerTweet');
     $('.centerTweet > .tweet:last-child').clone().css('display', 'none').prependTo('.timelineBOTTOM');
 
